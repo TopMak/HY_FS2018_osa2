e02fae7f92a24216385b963d80e7e4d3db835a9c
TopMak <26412430+TopMak@users.noreply.github.com>
2018-05-23 12:18:45 +0300

    Teht2.8 - added some "styles", names and numbers in table

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import './app.css';
 
 
 class App extends React.Component {
@@ -6,9 +7,14 @@ class App extends React.Component {
     super(props)
     this.state = {
       persons: [
-        { name: 'Arto Hellas' }
+        { name: 'Arto Hellas', number:'020-1236457' },
+        { name: 'Arto Kiuas', number:'020-8549867' },
+        { name: 'Tero Esa', number:'010-3256945' },
+        { name: 'Tiina Terävä', number:'050-9865478' },
+        { name: 'Meija Meikänen', number:'050-6547851' },
       ],
-      newName: ''
+      newName: '',
+      newNumber: ''
     }
   }
 
@@ -23,14 +29,17 @@ class App extends React.Component {
     //if((ehto === undefined) && (this.state.newName !== "")){
     if(eiArrayssa && eiTyhja){
       const uusiPerson = {
-        name: this.state.newName
+        name: this.state.newName,
+        number: this.state.newNumber
       }
 
       const uusiPersons = this.state.persons.concat(uusiPerson)
       //console.log(uusiPersons)
       this.setState({
         persons: uusiPersons,
-        newName: ''
+        newName: '',
+        newNumber: '',
+        suodatin: ''
       })
 
     } else{
@@ -44,24 +53,51 @@ class App extends React.Component {
     this.setState({ newName: event.target.value })
   }
 
+  asetaNewNumber = (event) => {
+    //console.log(event.target.value)
+    this.setState({ newNumber: event.target.value })
+  }
+
+  asetaSuodatin = (event) => {
+    console.log(event.target.value)
+    this.setState({ suodatin: event.target.value })
+  }
+
   render() {
     return (
-      <div>
+      <div className="container">
         <h2>Puhelinluettelo</h2>
-        <form onSubmit={this.lisaaNimi}>
-          <div>
-            nimi: <input
-            value={this.state.newName}
-            onChange={this.asetaNewName}
-            placeholder='Lisaa nimi...'
-            />
+
+
+        <div>
+          <div className="form">
+            <label className="labels">Etsi</label>
+            <input onChange={this.asetaSuodatin}/>
           </div>
-          <div>
+          <h3>Lisää uusi yhteystieto</h3>
+          <form className="form" onSubmit={this.lisaaNimi}>
+            <label className="labels">Nimi</label>
+              <input
+              value={this.state.newName}
+              onChange={this.asetaNewName}
+              placeholder='Lisaa nimi...'
+              />
+            <label className="labels">Numero</label>
+              <input
+              value={this.state.newNumber}
+              onChange={this.asetaNewNumber}
+              placeholder='Lisaa numero...'
+              />
+              <p></p>
             <button type="submit">lisää</button>
-          </div>
-        </form>
+          </form>
+        </div>
         <h2>Numerot</h2>
-          {this.state.persons.map( henkilo => <li key={henkilo.name}>{henkilo.name}</li>)}
+        <table className="taulu">
+          <tbody>
+            {this.state.persons.map( henkilo => <tr key={henkilo.name}><td>{henkilo.name}</td><td>{henkilo.number}</td></tr>)}
+          </tbody>
+        </table>
       </div>
     )
   }
