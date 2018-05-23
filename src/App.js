7641165c244fc9781970a044c3bf4de9f562d51a
TopMak <26412430+TopMak@users.noreply.github.com>
2018-05-23 11:03:58 +0300

    Teht2.7 - validation for empty and already existing person

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 
+
 class App extends React.Component {
   constructor(props) {
     super(props)
@@ -11,20 +12,56 @@ class App extends React.Component {
     }
   }
 
+  //https://reactjs.org/docs/forms.html
+  lisaaNimi = (event) => {
+    event.preventDefault()
+    //Find palauttaa undefined, jos ei löydy --> vaarallinen perhaps?
+    //let ehto = this.state.persons.find(nimi => this.state.newName === nimi.name)
+    let eiArrayssa = !(this.state.persons.some(henkilo => this.state.newName === henkilo.name))
+    let eiTyhja = (this.state.newName !== "")
+
+    //if((ehto === undefined) && (this.state.newName !== "")){
+    if(eiArrayssa && eiTyhja){
+      const uusiPerson = {
+        name: this.state.newName
+      }
+
+      const uusiPersons = this.state.persons.concat(uusiPerson)
+      //console.log(uusiPersons)
+      this.setState({
+        persons: uusiPersons,
+        newName: ''
+      })
+
+    } else{
+      alert("Nimi löytyy tai on tyhjä!")
+
+    }
+  }
+
+  asetaNewName = (event) => {
+    //console.log(event.target.value)
+    this.setState({ newName: event.target.value })
+  }
+
   render() {
     return (
       <div>
         <h2>Puhelinluettelo</h2>
-        <form>
+        <form onSubmit={this.lisaaNimi}>
           <div>
-            nimi: <input />
+            nimi: <input
+            value={this.state.newName}
+            onChange={this.asetaNewName}
+            placeholder='Lisaa nimi...'
+            />
           </div>
           <div>
             <button type="submit">lisää</button>
           </div>
         </form>
         <h2>Numerot</h2>
-        ...
+          {this.state.persons.map( henkilo => <li key={henkilo.name}>{henkilo.name}</li>)}
       </div>
     )
   }
